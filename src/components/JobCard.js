de002ef5e82f2dfc621f5efd92c47a25d861c789
An Nguyen <an.ndg95@gmail.com>
2024-03-16 14:49:40 +0700

    remove unused code

--- src/components/JobCard.js
+++ src/components/JobCard.js
@@ -1,11 +1,9 @@
 import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import {  CardActionArea } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
-import { ConstructionOutlined } from '@mui/icons-material';
 import SkillChip from './SkillChip';
 import { lime,purple } from '@mui/material/colors';
 import Button from '@mui/material/Button';
